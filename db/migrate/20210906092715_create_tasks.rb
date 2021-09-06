6ad423942ebac83bb73eb7932086f0c158625aef
BylevskijSV <sergeibylewskii@gmail.com>
2021-09-06 12:58:01 +0300

    add Task

---
+++ db/migrate/20210906092715_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :description
+      t.integer :status
+      t.belongs_to :user, foreign_key: true, type: :integer
+      t.integer :parent_id
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_05_200617) do
+ActiveRecord::Schema.define(version: 2021_09_06_092715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.integer "status"
+    t.integer "user_id"
+    t.integer "parent_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_09_05_200617) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
