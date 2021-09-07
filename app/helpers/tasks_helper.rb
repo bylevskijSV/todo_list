ec565967bf72edb558ac2991e143a587a4e9b5b9
BylevskijSV <sergeibylewskii@gmail.com>
2021-09-07 13:06:15 +0300

    add array STATUS, callback status_new, helper status_to_order

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,12 @@
 module TasksHelper
+  def status_to_string(status)
+    case status
+    when 1
+      Task::STATUS[0][0]
+    when 2
+      Task::STATUS[1][0]
+    when 3
+      Task::STATUS[2][0]
+    end
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,22 @@
 class Task < ApplicationRecord
   belongs_to :user
+  after_create :status_new
+
+  STATUS = [
+    ['new', 1],
+    ['in progress', 2],
+    ['done', 3]
+  ]
+
+  def complete!
+    self.status = 3
+    save
+  end
+
+  private
+
+  def status_new
+    self.status = 1
+  end
+
 end
