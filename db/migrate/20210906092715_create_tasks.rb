66a687f1db5c53525c3f3fddb2de1dcf36d19a58
BylevskijSV <sergeibylewskii@gmail.com>
2021-09-07 13:10:12 +0300

    add column status in tasks

--- db/migrate/20210906092715_create_tasks.rb
+++ db/migrate/20210906092715_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[6.1]
     create_table :tasks do |t|
       t.string :title
       t.string :description
-      t.integer :status
+      t.integer :status, default: '0'
       t.belongs_to :user, foreign_key: true, type: :integer
       t.integer :parent_id
       t.timestamps
