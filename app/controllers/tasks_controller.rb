3c964545d2c73ca65198672db9ec4970ce6b3818
BylevskijSV <sergeibylewskii@gmail.com>
2021-09-14 11:53:35 +0300

    add callback set_task, require_user_tasks. Fix path

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,20 @@
 class TasksController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index]
+  skip_before_action :authenticate_user!, only: %i[index no_authenticate]
+
+  before_action :set_task, only: %i[show edit update destroy complete]
+  before_action :require_user_tasks, only: %i[show edit update destroy]
 
   def index
     if user_signed_in?
-      @tasks = current_user.tasks.where.not(status: 3).order(:updated_at)
+      @tasks = current_user.tasks.where.not(status: 3).order(updated_at: :desc)
 
       @completed_tasks = current_user.tasks.where(status: 3).order(updated_at: :desc)
     else
-      render :no_authenticate_index
+      redirect_to no_authenticate_path
     end
   end
 
-  def no_authenticate_index
-  end
+  def no_authenticate; end
 
   def new
     @task = Task.new
@@ -20,16 +22,14 @@ class TasksController < ApplicationController
     @users = User.where.not(email: current_user.email)
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def create
     @task = assigned_task? ? Task.create(assigned_task_params) : current_user.tasks.create(task_params)
 
     if @task.errors.empty?
       flash[:notice] = 'Task was created successfully'
-      redirect_to root_path
+      redirect_to task_path(@task)
     else
       @users = User.where.not(email: current_user.email)
       render :edit
@@ -37,33 +37,28 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
     @users = User.where.not(email: current_user.email)
   end
 
   def update
-    @task = Task.find(params[:id])
-
     update_data = assigned_task? ? assigned_task_params : task_params
 
     if @task.update update_data
       flash[:notice] = 'Task was updated successfully'
       redirect_to task_path
     else
+      @users = User.where.not(email: current_user.email)
       render 'edit'
     end
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     flash[:alert] = 'Task was deleted successfully'
-    redirect_to root_path
+    redirect_to tasks_path
   end
 
   def complete
-    @task = Task.find(params[:id])
-
     @task.complete!
 
     redirect_to root_path
@@ -82,5 +77,16 @@ class TasksController < ApplicationController
 
   def assigned_task?
     params[:task][:assigned_task].to_i == 1
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def require_user_tasks
+    return unless current_user != @task.user
+
+    flash[:alert] = 'You can only edit, view, delete your own task'
+    redirect_to tasks_path
   end
 end
