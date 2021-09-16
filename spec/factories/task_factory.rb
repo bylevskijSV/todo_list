9fb6483da03413edf0d4c88cf584f20c867886b3
BylevskijSV <sergeibylewskii@gmail.com>
2021-09-16 23:47:24 +0300

    moved the factories to separate files

--- spec/factories/task_factory.rb
+++ spec/factories/task_factory.rb
@@ -1,11 +1,4 @@
 FactoryBot.define do
-  factory :user do
-    email { "test#{rand(100)}@mail.ru" }
-    password { 'test123' }
-    first_name { 'test1' }
-    last_name { 'test2' }
-  end
-
   factory :task do
     association :user, factory: :user
     title { 'Test task title in devise' }
