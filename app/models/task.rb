a2a831313396e0e328146d15f234e23c5e30c729
BylevskijSV <sergeibylewskii@gmail.com>
2021-09-17 02:38:40 +0300

    add NEW, IN_PROGRESS, DONE constants, "task_done?" predicate, replace save with update_column

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,27 +2,22 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :parent, class_name: 'User', optional: true
 
-  before_create :status_new
-
   delegate :full_name, to: :parent, prefix: true
 
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   validates :title, presence: true, length: { minimum: 3, maximum: 100 }
 
-  STATUS = [
-    ['new', 1],
-    ['in progress', 2],
-    ['done', 3]
+  STATUSES = [
+    NEW = 'new',
+    IN_PROGRESS = 'in progress',
+    DONE = 'done'
   ]
 
   def complete!
-    self.status = 3
-    save
+    update_column(:status, Task::DONE)
   end
 
-  private
-
-  def status_new
-    self.status = 1
+  def task_done?
+    status == Task::DONE
   end
 end
